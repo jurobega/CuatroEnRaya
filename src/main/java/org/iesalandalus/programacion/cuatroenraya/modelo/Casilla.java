c1dedb16283edfa6dd60fa3c9dc0cecda65c7d9c
jurob <jurobega2@gmail.com>
2025-01-22 13:35:31 +0100

    Clase Casilla terminado

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -0,0 +1,44 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+import java.util.Objects;
+
+public class Casilla {
+    private Ficha ficha;
+
+    public Casilla() {
+        this.ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws CuatroEnRayaExcepcion {
+        if (estaOcupado()) {
+            throw new CuatroEnRayaExcepcion("La casilla ya contiene una ficha.");
+        }
+        this.ficha = Objects.requireNonNull(ficha,"No se puede poner una ficha nula.");
+    }
+
+
+    public boolean estaOcupado() {
+        boolean ocupado;
+        if (getFicha() == null) {
+            ocupado = false;
+        }else {
+            ocupado = true;
+        }
+        return ocupado;
+    }
+
+    @Override
+    public String toString() {
+        String stringResultado;
+        if (estaOcupado()){
+            stringResultado = String.format("%s",this.ficha);
+        }else {
+            stringResultado = String.format(" ");
+        }
+        return String.format("%s",stringResultado);
+    }
+}
